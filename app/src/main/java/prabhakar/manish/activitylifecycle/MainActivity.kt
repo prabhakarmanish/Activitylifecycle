90313e46fc3fb4c5e8b42ef3311bde9157983010
prabhakarmanish <manishprabhakar100@gmail.com>
2024-09-11 14:00:05 +0530

    code added

--- app/src/main/java/prabhakar/manish/activitylifecycle/MainActivity.kt
+++ app/src/main/java/prabhakar/manish/activitylifecycle/MainActivity.kt
@@ -1,6 +1,8 @@
 package prabhakar.manish.activitylifecycle
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,6 +11,8 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("MainActivity","OnCreate")
+        Toast.makeText(this,"OnCreate",Toast.LENGTH_SHORT).show()
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -16,5 +20,41 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("MainActivity","OnStart")
+        Toast.makeText(this,"OnStart",Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("MainActivity","OnResume")
+        Toast.makeText(this,"OnResume",Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("MainActivity","OnPause")
+        Toast.makeText(this,"OnPause",Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("MainActivity","OnStop")
+        Toast.makeText(this,"OnStop",Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MainActivity","OnDestroy")
+        Toast.makeText(this,"OnDestroy",Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("MainActivity","OnRestart")
+        Toast.makeText(this,"onRestart",Toast.LENGTH_SHORT).show()
     }
 }
